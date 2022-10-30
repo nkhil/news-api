662391be28f4cbc84249d184b3e433d939628e8a
Nikhil Vijayan <nkhilv@gmail.com>
2022-10-30 22:32:00 +0000

    Use setInterval instead of node-cron/concurrently

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import app from './app'
+import * as fs from 'fs'
+
+import { getTopHeadlines } from './controller/news'
 
 app.listen(3000, () => {
   console.log('App listening...')
 })
+
+const TEN_MINUTES = 10 * (1000 * 60) // 60 seconds x 10 = 10 minutes
+
+setInterval(async () => {
+  console.log('timeout beyond time')
+  const data = await getTopHeadlines()
+  fs.writeFileSync('data.json', Buffer.from(JSON.stringify(data)))
+}, TEN_MINUTES)
