fb2a4c196304f7b7e0a97cfa98617f7853a5cade
Nikhil Vijayan <nkhilv@gmail.com>
2022-12-03 15:36:21 +0000

    Immediately fetch data on server startup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
-import app from './app'
 import * as fs from 'fs'
 
+import app from './app'
+import { cleanEnv, num } from 'envalid'
 import { getTopHeadlines } from './controller/news'
 
-app.listen(3000, () => {
+const env = cleanEnv(process.env, {
+  PORT: num(),
+})
+
+const PORT = env.PORT ?? 3000
+
+app.listen(PORT, () => {
   console.log('App listening...')
 })
 
 const TEN_MINUTES = 10 * (1000 * 60) // 60 seconds x 10 = 10 minutes
 
+// Immediately fetch data
+setImmediate(async () => {
+  const data = await getTopHeadlines()
+  fs.writeFileSync('data.json', Buffer.from(JSON.stringify(data)))
+})
+
+// Fetch new data every 10 minutes
 setInterval(async () => {
-  console.log('timeout beyond time')
   const data = await getTopHeadlines()
   fs.writeFileSync('data.json', Buffer.from(JSON.stringify(data)))
 }, TEN_MINUTES)
